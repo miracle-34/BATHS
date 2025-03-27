14cc246df5a9586ff6d37159522c59326d220555
Ohemaaa <aa.ohemaa10@yahoo.com>
2025-03-27 19:01:57 +0000

    Java code

---
+++ ManOWar.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class ManOWar here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ManOWar
+{
+    // instance variables - replace the example below with your own
+    private int noOfMarines;
+    private boolean doctor;
+    private int  noOfDecks;
+
+    /**
+     * Constructor for objects of class ManOWar
+     */
+    public ManOWar()
+    {
+        // initialise instance variables
+        ;
+    }
+
+
+    
+      public boolean hasDoctor()
+    {
+        // put your code here
+        return true; //f the ship has a doctor or not
+    }
+}
